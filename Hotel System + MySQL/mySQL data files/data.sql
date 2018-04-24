4cf932a012ef83bac2994d8d01e85b2e6b9b081f
Ivan Ivanov <ivan.petrov.ivanov@sciant.com>
2018-04-24 09:58:42 +0300

    Update sql initial

--- Hotel System + MySQL/mySQL data files/data.sql
+++ Hotel System + MySQL/mySQL data files/data.sql
@@ -15,3 +15,15 @@ INSERT INTO hotelsystem.users
 (id, username, hashed_pass, salt, is_admin)
 VALUES
 (2, 'test2', '0000', '1111', 1);
+
+CREATE TABLE hotels (
+id INT NOT NULL PRIMARY KEY,
+title VARCHAR(50),
+description VARCHAR(250),
+location VARCHAR(250),
+image_url VARCHAR(250),
+dateAdded DATE
+);
+
+insert into hotels(id, title, description, location, image_url, date_added)
+values(1, 'Metropolitan', 'Metropolitan', 'Sofia', 'http://www.sofia-guide.com/assets/metropolitan_hotel.jpg', '2018-04-24');
