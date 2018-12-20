6dcefca684659b2cbb5f783886f751a0d01d50a2
vancho <van_cho@abv.bg>
2018-12-20 10:54:36 +0200

    Finished payment component.

--- Ticket Store/client/src/app/components/user/payment/payment.component.ts
+++ Ticket Store/client/src/app/components/user/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
+  buy(): void {
+    this.toastr.info('Thanks for buying these tickets. This functionality is not available.');
+  }
 }
